70540287a0f8a4b7ce7d8fd820ca3f7c80ee6631
Pratik Chudasama <pratikchudasama03@gmail.com>
2025-05-02 17:32:00 +0530

    fix

--- database/migrations/2025_05_01_090124_update_personal_access_tokens_table_for_uuid.php
+++ database/migrations/2025_05_01_090124_update_personal_access_tokens_table_for_uuid.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -10,22 +9,12 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up():void
     {
         Schema::table('personal_access_tokens', function (Blueprint $table) {
-            $table->uuid('tokenable_uuid')->nullable();
-        });
-    
-        // Optional: if there's a way to map bigint IDs to UUIDs, do it here
-        // DB::table('personal_access_tokens')->update([...]);
-    
-        Schema::table('personal_access_tokens', function (Blueprint $table) {
-            $table->dropColumn('tokenable_id');
-            $table->renameColumn('tokenable_uuid', 'tokenable_id');
+            $table->uuid('tokenable_id')->change();
         });
     }
-    
-    
 
     public function down():void
     {
